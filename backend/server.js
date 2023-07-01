39ae9be078d24e364343cf57cb96af9cfa023170
Matt Hammond <mjhammond94@gmail.com>
2023-07-01 16:23:53 +0100

    Install Mongoose - Add models directory - Add visitor model - Add connection in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 require('dotenv').config()
 
 const express = require('express')
+const mongoose = require('mongoose')
 const visitorRoutes = require('./routes/crudRouter')
-
 const CRUDapp = express()
+
+CRUDapp.use(express.json())
 
 CRUDapp.use((req, res, next) => {
   console.log(req.path, req.method)
@@ -12,6 +14,13 @@ CRUDapp.use((req, res, next) => {
 
 CRUDapp.use('/homepage', visitorRoutes)
 
-CRUDapp.listen(process.env.PORT, () => {
-  console.log('Listening on port', process.env.PORT)
-})
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    CRUDapp.listen(process.env.PORT, () => {
+      console.log('Connected to DB and Listening on port', process.env.PORT)
+    })
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+
