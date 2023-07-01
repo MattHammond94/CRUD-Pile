bdd7ef7ddafc799fa2c3b1f52c3ff74b015004f7
Matt Hammond <mjhammond94@gmail.com>
2023-07-01 14:40:46 +0100

    Add dotenv - Add ENV variables to .env file - Ignore .env file - define original routes

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,25 @@
+require('dotenv').config()
+
 const express = require('express')
 
 const CRUDapp = express()
 
-CRUDapp.listen(420, () => {
-  console.log('Listening on port 420')
+CRUDapp.get('/', (req, res) => {
+  res.json({ message: 'Welcome to the terrordome' })
+})
+
+CRUDapp.post('/', (req, res) => {
+  res.json({ message: 'This is a post request' })
+})
+
+CRUDapp.delete('/', (req, res) => {
+  res.json({ message: 'This is a delete request' })
+})
+
+CRUDapp.patch('/', (req, res) => {
+  res.json({ message: 'This is a update request' })
+})
+
+CRUDapp.listen(process.env.PORT, () => {
+  console.log('Listening on port', process.env.PORT)
 })
